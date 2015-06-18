37979ec0d3ca155ef95ee7faa36b079c5735b7a4
tjane <1176998162@qq.com>
2015-06-18 09:09:51 +0800

    first commitment in ubuntu

---
+++ hello.c
@@ -0,0 +1,11 @@
+// Created By:      Demo <demo@demo.com>
+// Created Time:    2015-06-18 08:56:45
+// Modified Time:   2015-06-18 08:58:04
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    print("Hello World\n");
+    return 0;
+}
+
